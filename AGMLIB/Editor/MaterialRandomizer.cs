d62ce5992e9c03ee32c83bb3339f90ff825a1ece
agm-114 <agm.114.email@gmail.com>
2024-09-28 12:46:13 +0530

    Material randomizer.

---
+++ AGMLIB/Editor/MaterialRandomizer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Random = System.Random;
+
+namespace AGMLIB.Editor
+{
+    public class MaterialRandomizer : MonoBehaviour
+    {
+        public List<Material> Materials = new(2);
+        public MeshRenderer Target;
+
+        public void Awake()
+        {
+            //Debug.LogError("Randomizing: " + Materials.Count());
+            Target.material = Materials[new Random().Next(Materials.Count())];
+        }
+    }
+}
