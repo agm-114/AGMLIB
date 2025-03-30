72673427b32a679ed91af325a6f22ec497259a25
agm-114 <agm.114.email@gmail.com>
2025-03-30 14:03:23 +0530

    misc transform changes

--- AGMLIB/Dynamic Systems/DynamicTransform.cs
+++ AGMLIB/Dynamic Systems/DynamicTransform.cs
@@ -1,4 +1,6 @@
-﻿public class DynamicTransform : ActiveSettings
+﻿using Telepathy;
+
+public class DynamicTransform : ActiveSettings
 {
     // Start is called before the first frame update
     [Space]
@@ -7,11 +9,13 @@
     public Transform Start;
     public Transform End;
     public Transform Target;
+    public float MaxTime = 1f;
     public AnimationCurve AnimationCurve = new(new Keyframe(0, 0), new Keyframe(1, 1));
 
     public float SlerpVlaue => Mathf.Clamp(AnimationCurve.Evaluate(accum), 0, 1);
     public float EndValue => 1 - SlerpVlaue;
     private float accum = 0f;
+    
     //public GameObject target;
 
     // Update is called once per frame
@@ -20,19 +24,29 @@
         base.FixedUpdate();
         if (active)
         {
-            accum += Time.deltaTime;
-            //Common.Trace(this.gameObject, "accum");
-        }
+            //Common.Trace(this.gameObject, $"p+{accum}, {Target.transform.position}, {Target.transform.rotation}");
 
+            accum += Time.fixedDeltaTime;
+            //Common.Trace(this.gameObject, $"a+{accum}, {Target.transform.position}, {Target.transform.rotation}");
+
+            //C
+        }
         else
         {
-            accum -= Time.deltaTime;
+            //Common.Trace(this.gameObject, $"p-{accum}, {Target.transform.position}, {Target.transform.rotation}");
+
+            accum -= Time.fixedDeltaTime;
+            //Common.Trace(this.gameObject, $"a-{accum}, {Target.transform.position}, {Target.transform.rotation}");
+
             //Common.Trace(this.gameObject, "accum dead");
         }
 
-
-        if(accum < 0f)
+        if (accum < 0f)
             accum = 0f;
+        else if(accum > MaxTime)
+            accum = MaxTime;
+        //Common.Trace(this.gameObject, $"{accum}, {Target.transform.position}, {Target.transform.rotation}");
+
         Target.transform.position = Vector3.Slerp(Start.transform.position, End.transform.position, SlerpVlaue);
         Target.transform.rotation = Quaternion.Slerp(Start.transform.rotation, End.transform.rotation, SlerpVlaue);
     }
