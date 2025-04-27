f55ed5c7789c325b73c79e6b6698228f02fddb69
agm-114 <agm.114.email@gmail.com>
2025-04-27 13:12:26 +0530

    muzzle list bugfixes

--- AGMLIB/Generic Gameplay/Muzzles/MuzzleList.cs
+++ AGMLIB/Generic Gameplay/Muzzles/MuzzleList.cs
@@ -1,4 +1,5 @@
-﻿using Steamworks;
+﻿using Ships;
+using Steamworks;
 using System;
 
 namespace AGMLIB.Generic_Gameplay.Discrete
@@ -31,15 +32,15 @@ namespace AGMLIB.Generic_Gameplay.Discrete
             {
                 return true;
             }
-            return _muzzles[0].SimMethod == ammo.SimMethod && CompatibleAmmoTags.Any((MunitionTags x) => x.Equals(ammo.Tags));
+            return CompatibleAmmoTags.Any((MunitionTags x) => x.Equals(ammo.Tags));
         }
 
         public void Awake()
         {
-            Common.Hint(this.gameObject, "muzzle");
+            //Common.Hint(this.gameObject, "muzzle");
             foreach (var muzzle in Muzzles)
             {
-                Common.Hint("Spawn Muzzle" + nameof(muzzle));
+                //Common.Hint("Spawn Muzzle" + nameof(muzzle));
 
             }
         }
@@ -55,9 +56,9 @@ namespace AGMLIB.Generic_Gameplay.Discrete
             Common.LogPatch();
             List<MuzzleList> muzzleLists = __instance.GetComponentsInChildren<MuzzleList>().ToList() ?? new List<MuzzleList>();
             // Check if the condition to skip the function is met
-            if (muzzleLists.Count > 0)
+            if (muzzleLists.Count >= 0)
             {
-                Common.Hint("No Lists");
+                //Common.Hint("No Lists");
 
                 num = 0;
                 return Common.RunFunction;
@@ -65,55 +66,136 @@ namespace AGMLIB.Generic_Gameplay.Discrete
             if (muzzleLists.Any(mlist => mlist.IsAmmoCompatible(__instance.SelectedAmmoType)))
             {
                 __result = muzzleLists.Find(mlist => mlist.IsAmmoCompatible(__instance.SelectedAmmoType)).GetNextMuzzle(out num);
+                if (__instance.SelectedAmmoType.SimMethod != __result.SimMethod)
+                    Common.Hint($"Possible Sim Method Issue Ammo: {__instance.SelectedAmmoType.SimMethod} Muzzle: {__result.SimMethod}" ) ;
+
+
                 Common.Hint("Returning Muzzle" + nameof(__result));
 
                 return Common.SkipFunction;
             }
             else
             {
-                //Common.Hint("Hey none of your ammo lists are compatible with the ammo you want to use");
+                Common.Hint("Hey none of your muzzle lists are compatible with the ammo you want to use");
+                Common.Hint($"Ammo C {__instance.SelectedAmmoType.Tags.Class} S {__instance.SelectedAmmoType.Tags.Subclass}");
+                foreach(MuzzleList muzzleList in muzzleLists)
+                {
+                    Common.Hint("List");
+                    foreach(MunitionTags tags in muzzleList.CompatibleAmmoTags)
+                    {
+                        Common.Hint($"Ammo C {tags.Class} S {tags.Subclass}");
+                    }
+                }
             }
             num = 0;
             return Common.RunFunction;
         }
     }
 
-    [HarmonyPatch(typeof(ContinuousWeaponComponent), "StartFiring")]
-    public class ContinuousWeaponComponentStartFiring
+    public class DummyMuzzle : Muzzle
     {
-        protected static Muzzle[] _muzzles = null;
+        // Dummy simulation method
+        public override MunitionSimulationMethod SimMethod => MunitionSimulationMethod.SteppedRaycast;
+
+        public override void Fire()
+        {
+            // Do nothing
+        }
+
+        public override void FireEffect()
+        {
+            // Do nothing
+        }
+
+        public override void GetAccuracyStat(List<(string, string)> rows, float[] distances)
+        {
+            // No accuracy data to report
+        }
+
+        public override void StopFire()
+        {
+            // Do nothing
+        }
+
+        public override void StopFireEffect()
+        {
+            // Do nothing
+        }
+
+        public override void TriggerHitEffect(HitResult hit, Vector3 position, Quaternion rotation)
+        {
+            // Do nothing
+        }
+    }
+
+    public class MuzzleState : MonoBehaviour
+    {
+        public Muzzle[] _muzzles = null;
+    }
+
+    [HarmonyPatch(typeof(ContinuousWeaponComponent), "StartFiring")]
+    public class ContinuousWeaponComponentStartFiring : MonoBehaviour
+    {
 
         static void Prefix(ContinuousWeaponComponent __instance)
         {
             Common.LogPatch();
+            Postfix(__instance);
             List<MuzzleList> muzzleLists = __instance.GetComponentsInChildren<MuzzleList>().ToList() ?? new List<MuzzleList>();
             // Check if the condition to skip the function is met
             if (muzzleLists.Count <= 0)
             {
                 //Common.Hint("No Lists");
-                ;
                 return;
             }
+            MuzzleState state = __instance.GetComponentInChildren<MuzzleState>() ?? __instance.gameObject.AddComponent<MuzzleState>();
+            if (state._muzzles == null)
+            {
+                state._muzzles = Common.GetVal<Muzzle[]>(__instance, "_muzzles");
+            }
+            Muzzle[] _muzzles = state._muzzles;
+            //Common.SetVal(__instance, "_muzzles", _muzzles);
+            //_muzzles = null;
             if (muzzleLists.Any(mlist => mlist.IsAmmoCompatible(__instance.SelectedAmmoType)))
             {
-                _muzzles =  Common.GetVal<Muzzle[]>(__instance, "_muzzles");
+                Common.Hint("Found Good List");
+
+                
+
                 Muzzle[] _tempmuzzles = muzzleLists.Find(mlist => mlist.IsAmmoCompatible(__instance.SelectedAmmoType)).Muzzles;
+                if (__instance.SelectedAmmoType.SimMethod != _tempmuzzles[0].SimMethod)
+                    Common.Hint($"Possible Sim Method Issue Ammo: {__instance.SelectedAmmoType.SimMethod} Muzzle: {_tempmuzzles[0].SimMethod}");
+                var filteredMuzzles = new Muzzle[_muzzles.Length];
+                for (int i = 0; i < _muzzles.Length; i++)
+                {
+                    // Check if the original muzzle exists in the compatible list
+                    if (_tempmuzzles.Contains(_muzzles[i]))
+                        filteredMuzzles[i] = _muzzles[i];
+                    else
+                        filteredMuzzles[i] = new DummyMuzzle();
+                }
                 Common.SetVal<Muzzle[]>(__instance, "_muzzles", _tempmuzzles);
                 return;
             }
             else
             {
-                Common.Hint("Hey none of your ammo lists are compatible with the ammo you want to use");
+                Common.Hint("Hey none of your muzzle lists are compatible with the ammo you want to use");
+                Common.Hint($"Ammo C {__instance.SelectedAmmoType.Tags.Class} S {__instance.SelectedAmmoType.Tags.Subclass}");
+                foreach (MuzzleList muzzleList in muzzleLists)
+                {
+                    Common.Hint("List");
+                    foreach (MunitionTags tags in muzzleList.CompatibleAmmoTags)
+                    {
+                        Common.Hint($"Ammo C {tags.Class} S {tags.Subclass}");
+                    }
+                }
             }
         }
 
         static void Postfix(ContinuousWeaponComponent __instance)
         {
             Common.LogPatch();
-            if (_muzzles == null)
-                return;
-            Common.SetVal(__instance, "_muzzles", _muzzles);
-            _muzzles = null;
+ 
         }
     }
 }
