95808a982885a93efaa77396092d65c89cf75bcc
agm-114 <agm.114.email@gmail.com>
2025-04-27 13:07:46 +0530

    misc radars

---
+++ AGMLIB/Generic Gameplay/Ewar/TurretedInternalActiveSensorComponent - Copy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility;
+using static Ships.SensorTurretComponent;
+
+
+public class TurretedInternalActiveSensorComponent : BaseActiveSensorComponent
+{
+    [SerializeField]
+    private TurretController _turretControl;
+
+    protected override SensorCategory _sensorCategory => SensorCategory.GeneralSearch;
+    protected override void SocketSet()
+    {
+        base.SocketSet();
+        _turretControl.SetLimits(base.Socket.TraverseLimits, base.Socket.ForwardTraverseLimits);
+    }
+    public override bool EmissionsVisibleFromPosition(Vector3 sensorPosition) => _turretControl.TargetWithinLimits(sensorPosition);
+    public override bool CanTrainOnTarget(Vector3 targetPos) => _turretControl.TargetWithinLimits(targetPos);
+    protected override bool CanJammerHitApertureInternal(Vector3 jammingDirection) => _turretControl.TargetWithinLimits(base.transform.root.TransformDirection(-jammingDirection));
+    protected override bool CanSeeSignature(IActiveSignature sig)
+    {
+        if (!_turretControl.TargetWithinLimits(sig.Position))
+        {
+            return false;
+        }
+        return base.CanSeeSignature(sig);
+    }
+
+}
---
+++ AGMLIB/Generic Gameplay/Ewar/TurretedInternalActiveSensorComponent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility;
+using static Ships.SensorTurretComponent;
+
+
+public class InternalSectorActiveSensorComponent : BaseActiveSensorComponent
+{
+    [SerializeField]
+    private Sector _coverage;
+
+    protected override SensorCategory _sensorCategory => SensorCategory.GeneralSearch;
+
+    public override bool EmissionsVisibleFromPosition(Vector3 sensorPosition)
+    {
+        Vector3 toSensor = base._provider.ProviderPosition.To(sensorPosition);
+        Vector3 localDirection = base.transform.root.InverseTransformDirection(toSensor.normalized);
+        if ((_coverage & localDirection.ClassifySector()) == 0)
+        {
+            return false;
+        }
+        return base.EmissionsVisibleFromPosition(sensorPosition);
+    }
+
+    protected override bool CanSeeSignature(IActiveSignature sig)
+    {
+        Vector3 toSig = base.transform.position.To(sig.Position);
+        Vector3 localPosition = base.transform.root.InverseTransformDirection(toSig);
+        if ((_coverage & localPosition.ClassifySector()) == 0)
+        {
+            return false;
+        }
+        return base.CanSeeSignature(sig);
+    }
+
+    protected override bool CanJammerHitApertureInternal(Vector3 jammingDirection)
+    {
+        Vector3 localDirection = base.transform.root.InverseTransformDirection(-jammingDirection);
+        return (_coverage & localDirection.ClassifySector()) != 0;
+    }
+
+    public override bool CanTrainOnTarget(Vector3 targetPos)
+    {
+        Vector3 toSig = base._provider.ProviderPosition.To(targetPos);
+        Vector3 toSigLocal = base.transform.InverseTransformDirection(toSig).normalized;
+        return (_coverage & toSigLocal.ClassifySector()) != 0;
+    }
+}
