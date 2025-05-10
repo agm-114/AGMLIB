68235628dc917ee931371498de924ac02816557f
agm-114 <46271745+agm-114@users.noreply.github.com>
2025-05-10 09:34:53 +0530

    bugfix

---
+++ AGMLIB/Server/CarrierSigPatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+[HarmonyPatch(typeof(BoxSignature), "Awake")]
+class BoxSignatureAwake
+{
+
+    static void Prefix(BoxSignatureAwake __instance)
+    {
+        BoxCollider _collider = Common.GetVal<BoxCollider>(__instance, "_collider");
+        if (_collider == null)
+        {
+            return;
+        }
+        Bounds  _sigSize = new Bounds(Vector3.zero, _collider.bounds.size);
+        Common.SetVal(__instance, "_sigSize", _sigSize);
+        _collider.enabled = false;
+    }
+
+
+
+
+
+}
