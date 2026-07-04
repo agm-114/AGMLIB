1b2d3797501d6db024a7214a5a70947884e450a2
agm-114 <46271745+agm-114@users.noreply.github.com>
2026-07-04 22:26:46 +0530

    Imodular updates

--- AGMLIB/Munitions/ModularMissile/ModularSystems/IModular.cs
+++ AGMLIB/Munitions/ModularMissile/ModularSystems/IModular.cs
@@ -8,6 +8,16 @@ public interface ILimited
     public Dictionary<string, int> RestrictedOptions { get; }
 }
 
+public interface IOnWeaponAmmoChanged
+{
+    void OnWeaponAmmoChanged(WeaponComponent weapon, bool wasReloading);
+}
+
+public interface IOnDiscreteWeaponCheckFire
+{
+    void OnDiscreteWeaponCheckFire(DiscreteWeaponComponent weapon);
+}
+
 public interface IOnDamageableImpact
 {
     void OnDamageableImpact(LightweightKineticShell shell, LightweightKineticMunitionContainer attachedTo, IDamageable hitObject, MunitionHitInfo hitInfo, bool trigger, HitResult hitRes, float damageDone, bool targetDestroyed);
@@ -18,13 +28,23 @@ public interface IOnGenericImpact
     void OnGenericImpact(LightweightKineticShell shell, LightweightKineticMunitionContainer attachedTo, MunitionHitInfo hitInfo, bool trigger, HitResult hitRes);
 }
 
-public interface IOnDiscreteWeaponCheckFire
+public static class Modular
 {
-    void OnDiscreteWeaponCheckFire(DiscreteWeaponComponent weapon);
-}
+    public static IEnumerable<T> SelectedAmmoModules<T>(WeaponComponent weapon)
+    {
+        if (weapon?.SelectedAmmoType is not IModular modular || modular.Modules == null)
+        {
+            return Enumerable.Empty<T>();
+        }
 
-public class Modular
-{
+        return modular.Modules.OfType<T>();
+    }
+
+    public static T SelectedAmmoModule<T>(WeaponComponent weapon) where T : class
+    {
+        return SelectedAmmoModules<T>(weapon).FirstOrDefault();
+    }
+
     public static IFilterIndexed FindIndexedFilter(List<IFilterIndexed> filters, int index = -1)
     {
         if (filters == null || filters.Count <= 0)
@@ -45,6 +65,45 @@ public class Modular
     public static IFilterIndexed FindIndexedFilter(IEnumerable<IFilterIndexed> filters, int index = -1) => FindIndexedFilter(filters.ToList(), index);
 
     public static IFilterIndexed Default => ScriptableObject.CreateInstance<ScriptableFilter>();
+}
+
+[HarmonyPatch(typeof(WeaponComponent), nameof(WeaponComponent.ChangeAmmoType))]
+public class WeaponComponentChangeAmmoTypeModuleCallbacks
+{
+    static void Prefix(WeaponComponent __instance, out bool __state)
+    {
+        __state = __instance is DiscreteWeaponComponent discrete && Common.GetVal<bool>(discrete, "_reloading");
+    }
+
+    static void Postfix(WeaponComponent __instance, bool __result, bool __state)
+    {
+        if (!__result || __instance?.SelectedAmmoType is not IModular modular || modular.Modules == null)
+        {
+            return;
+        }
+
+        foreach (IOnWeaponAmmoChanged module in modular.Modules.OfType<IOnWeaponAmmoChanged>())
+        {
+            module.OnWeaponAmmoChanged(__instance, __state);
+        }
+    }
+}
+
+[HarmonyPatch(typeof(DiscreteWeaponComponent), "CheckFire")]
+public class DiscreteWeaponComponentCheckFireModuleCallbacks
+{
+    static void Prefix(DiscreteWeaponComponent __instance)
+    {
+        if (__instance?.SelectedAmmoType is not IModular modular || modular.Modules == null)
+        {
+            return;
+        }
+
+        foreach (IOnDiscreteWeaponCheckFire module in modular.Modules.OfType<IOnDiscreteWeaponCheckFire>())
+        {
+            module.OnDiscreteWeaponCheckFire(__instance);
+        }
+    }
 }
 
 [HarmonyPatch(typeof(LightweightKineticShell), nameof(LightweightKineticShell.DamageableImpact))]
@@ -77,23 +136,6 @@ public class LightweightKineticShellGenericImpactModuleCallbacks
         foreach (IOnGenericImpact module in modular.Modules.OfType<IOnGenericImpact>())
         {
             module.OnGenericImpact(__instance, attachedTo, hitInfo, trigger, hitRes);
-        }
-    }
-}
-
-[HarmonyPatch(typeof(DiscreteWeaponComponent), "CheckFire")]
-public class DiscreteWeaponComponentCheckFireModuleCallbacks
-{
-    static void Prefix(DiscreteWeaponComponent __instance)
-    {
-        if (__instance?.SelectedAmmoType is not IModular modular || modular.Modules == null)
-        {
-            return;
-        }
-
-        foreach (IOnDiscreteWeaponCheckFire module in modular.Modules.OfType<IOnDiscreteWeaponCheckFire>())
-        {
-            module.OnDiscreteWeaponCheckFire(__instance);
         }
     }
 }
