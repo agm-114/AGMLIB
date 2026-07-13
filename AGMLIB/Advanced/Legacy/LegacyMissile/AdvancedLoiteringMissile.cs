1068f03b79e1d7d641f49faa7b395643c612a544
agm-114 <46271745+agm-114@users.noreply.github.com>
2026-07-13 21:56:14 +0530

    modular mines

---
+++ AGMLIB/Advanced/Legacy/LegacyMissile/AdvancedLoiteringMissile.cs
@@ -0,0 +1,193 @@
+﻿using System.Reflection;
+using Munitions;
+using SmallCraft;
+
+public class AdvancedLoiteringMissile : LoiteringMissile
+{
+    private static readonly FieldInfo ActivationTriggerField = typeof(LoiteringMissile).GetField(
+        "_activationTrigger",
+        BindingFlags.Instance | BindingFlags.NonPublic);
+
+    private static readonly MethodInfo TriggerAttackMethod = typeof(LoiteringMissile).GetMethod(
+        "TriggerAttack",
+        BindingFlags.Instance | BindingFlags.NonPublic);
+
+    [Header("Advanced Magnetic Fuse")]
+    [Tooltip("Enables the replacement proximity and sensor fuse controller.")]
+    public bool MagneticFuse = true;
+
+    [Tooltip("Maximum distance at which a physical target can activate the munition.")]
+    [Min(0f)]
+    [SerializeField]
+    private float _magneticFuseRange = 1000f;
+
+    [Tooltip("Allows hostile ships to activate the magnetic fuse.")]
+    public bool ProximityShips = true;
+
+    [Tooltip("Allows hostile fighters and other small craft to activate the magnetic fuse.")]
+    public bool ProximityFighters = true;
+
+    [Tooltip("Allows hostile missiles to activate the magnetic fuse.")]
+    public bool ProximityMissiles = false;
+
+    [Tooltip("Legacy missile seekers that may activate the munition when they acquire a target.")]
+    public List<MissileSeeker> SensorFuses = new();
+
+    [Tooltip("Physics layers searched by the magnetic fuse.")]
+    [SerializeField]
+    private LayerMask _magneticFuseLayers = ~0;
+
+    [Tooltip("Requires an unobstructed line between the munition and a physical proximity target.")]
+    [SerializeField]
+    private bool _requireFuseLineOfSight = true;
+
+    [Tooltip("Physics layers that obstruct the magnetic fuse.")]
+    [SerializeField]
+    private LayerMask _fuseObstructionLayers = 512;
+
+    private readonly Collider[] _magneticFuseResults = new Collider[64];
+    private SphereCollider _stockActivationTrigger;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _stockActivationTrigger = ActivationTriggerField?.GetValue(this) as SphereCollider;
+        DisableStockActivationTrigger();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        DisableStockActivationTrigger();
+
+        if (isServer && MagneticFuse && TryGetFuseTarget(out Vector3 targetPosition))
+        {
+            TriggerAttack(targetPosition);
+        }
+    }
+
+    protected override void OnImbued(IImbuedObjectSource platform)
+    {
+        base.OnImbued(platform);
+        foreach (MissileSeeker sensor in SensorFuses)
+        {
+            if (sensor == null)
+            {
+                continue;
+            }
+
+            sensor.SetOwner(platform.OwnedBy);
+            sensor.SetLaunchingPlatform(platform);
+        }
+    }
+
+    protected override void OnUnpooled()
+    {
+        base.OnUnpooled();
+        foreach (MissileSeeker sensor in SensorFuses)
+        {
+            sensor?.ResetSeeker();
+        }
+        DisableStockActivationTrigger();
+    }
+
+    private bool TryGetFuseTarget(out Vector3 targetPosition)
+    {
+        if (TryGetSensorFuseTarget(out targetPosition))
+        {
+            return true;
+        }
+
+        if (!ProximityShips && !ProximityFighters && !ProximityMissiles)
+        {
+            targetPosition = default;
+            return false;
+        }
+
+        int count = Physics.OverlapSphereNonAlloc(
+            transform.position,
+            _magneticFuseRange,
+            _magneticFuseResults,
+            _magneticFuseLayers,
+            QueryTriggerInteraction.Collide);
+
+        for (int i = 0; i < count; i++)
+        {
+            Transform targetRoot = _magneticFuseResults[i]?.transform.root;
+            if (targetRoot == null)
+            {
+                continue;
+            }
+
+            if (ProximityShips)
+            {
+                ShipController ship = targetRoot.GetComponent<ShipController>();
+                if (ship != null && !ship.IsEliminated && IsHostile(ship.OwnedBy) && HasFuseLineOfSight(ship.Position))
+                {
+                    targetPosition = ship.Position;
+                    return true;
+                }
+            }
+
+            if (ProximityFighters)
+            {
+                Spacecraft fighter = targetRoot.GetComponent<Spacecraft>();
+                if (fighter != null && IsHostile(fighter.OwnedBy) && HasFuseLineOfSight(fighter.Position))
+                {
+                    targetPosition = fighter.Position;
+                    return true;
+                }
+            }
+
+            if (ProximityMissiles)
+            {
+                Missile missile = targetRoot.GetComponent<Missile>();
+                if (missile != null && missile != this && IsHostile(missile.OwnedBy) && HasFuseLineOfSight(missile.transform.position))
+                {
+                    targetPosition = missile.transform.position;
+                    return true;
+                }
+            }
+        }
+
+        targetPosition = default;
+        return false;
+    }
+
+    private bool TryGetSensorFuseTarget(out Vector3 targetPosition)
+    {
+        foreach (MissileSeeker sensor in SensorFuses)
+        {
+            if (sensor != null && sensor.SearchForTarget(out targetPosition, out _, out _))
+            {
+                return true;
+            }
+        }
+
+        targetPosition = default;
+        return false;
+    }
+
+    private bool IsHostile(IPlayer targetOwner)
+    {
+        return targetOwner != null && !IFFExtensions.GetIFF(targetOwner, OwnedBy).IsFriendly();
+    }
+
+    private bool HasFuseLineOfSight(Vector3 targetPosition)
+    {
+        return !_requireFuseLineOfSight || !Physics.Linecast(transform.position, targetPosition, _fuseObstructionLayers);
+    }
+
+    private void DisableStockActivationTrigger()
+    {
+        if (_stockActivationTrigger != null)
+        {
+            _stockActivationTrigger.enabled = false;
+        }
+    }
+
+    private void TriggerAttack(Vector3 targetPosition)
+    {
+        TriggerAttackMethod?.Invoke(this, new object[] { targetPosition, true });
+    }
+}
---
+++ AGMLIB/Munitions/ModularMissile/LoiteringModularMissile.cs
@@ -0,0 +1,277 @@
+﻿using Munitions.ModularMissiles;
+using Ships;
+using Munitions.ModularMissiles.Runtime.Seekers;
+using SmallCraft;
+
+public class LoiteringModularMissile : ModularMissile
+{
+    private enum LoiteringFlightPhase
+    {
+        Inactive,
+        Flyout,
+        Arming,
+        Loitering,
+        Attacking
+    }
+
+    [Header("Loitering Munition")]
+    [Tooltip("Time spent following the programmed missile course before entering the loiter state.")]
+    [Min(0f)]
+    [SerializeField]
+    private float _flyoutTime = 5f;
+
+    [Tooltip("Delay between completing flyout and becoming able to detect targets.")]
+    [Min(0f)]
+    [SerializeField]
+    private float _armingDelay = 2f;
+
+    [Tooltip("Maximum time spent waiting for a target. Zero gives unlimited loiter time.")]
+    [Min(0f)]
+    [SerializeField]
+    private float _loiterLifetime = 120f;
+
+    [Tooltip("Range within which a hostile ship wakes the missile.")]
+    [Min(0f)]
+    [SerializeField]
+    private float _activationRange = 1000f;
+
+    [Tooltip("Enables proximity activation while the missile is loitering.")]
+    public bool MagneticFuse = true;
+
+    [Tooltip("Allows the magnetic fuse to be triggered by hostile ships.")]
+    public bool ProximityShips = true;
+
+    [Tooltip("Allows the magnetic fuse to be triggered by hostile fighters and other small craft.")]
+    public bool ProximityFighters = true;
+
+    [Tooltip("Allows the magnetic fuse to be triggered by hostile missiles.")]
+    public bool ProximityMissiles = false;
+
+    [Tooltip("One entry per runtime seeker. Enabled entries allow that seeker to trigger the magnetic fuse.")]
+    public List<bool> SeekerFuses = new();
+
+    [Tooltip("Physics layers searched for ships. Leave as Everything unless the missile prefab uses a narrower mask.")]
+    [SerializeField]
+    private LayerMask _activationLayers = ~0;
+
+    [Tooltip("Do not wake for targets hidden behind map geometry.")]
+    [SerializeField]
+    private bool _requireLineOfSight = true;
+
+    [Tooltip("Layers that obstruct activation line of sight.")]
+    [SerializeField]
+    private LayerMask _obstructionLayers = 512;
+
+    [Tooltip("Rigidbody drag used while the missile is arming or loitering.")]
+    [Min(0f)]
+    [SerializeField]
+    private float _loiterDrag = 1f;
+
+    private readonly Collider[] _activationResults = new Collider[64];
+    private LoiteringFlightPhase _phase;
+    private float _phaseTime;
+    private float _defaultDrag;
+
+    public bool IsLoitering => _phase == LoiteringFlightPhase.Loitering;
+
+    public override bool SupportsPositionTargeting => true;
+
+    public override bool SupportsTrackTargeting => false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _defaultDrag = _body.drag;
+        ResetLoiteringState();
+    }
+
+    protected override void LaunchInternal(ILaunchingPlatform platform, bool forceHotLaunch, bool immediateSearching)
+    {
+        base.LaunchInternal(platform, forceHotLaunch, immediateSearching);
+        ExtendSeekerFuseList();
+        EnterPhase(immediateSearching ? LoiteringFlightPhase.Attacking : LoiteringFlightPhase.Flyout);
+    }
+
+    protected override void FixedUpdate()
+    {
+        _phaseTime += Time.fixedDeltaTime;
+
+        switch (_phase)
+        {
+            case LoiteringFlightPhase.Flyout:
+                base.FixedUpdate();
+                if (_phaseTime >= _flyoutTime)
+                {
+                    EnterPhase(LoiteringFlightPhase.Arming);
+                }
+                break;
+
+            case LoiteringFlightPhase.Arming:
+                if (_phaseTime >= _armingDelay)
+                {
+                    EnterPhase(LoiteringFlightPhase.Loitering);
+                }
+                break;
+
+            case LoiteringFlightPhase.Loitering:
+                if (MagneticFuse && TryTriggerMagneticFuse())
+                {
+                    EnterPhase(LoiteringFlightPhase.Attacking);
+                }
+                else if (_loiterLifetime > 0f && _phaseTime >= _loiterLifetime && isServer)
+                {
+                    SelfDestruct();
+                }
+                break;
+
+            case LoiteringFlightPhase.Attacking:
+            case LoiteringFlightPhase.Inactive:
+            default:
+                base.FixedUpdate();
+                break;
+        }
+    }
+
+    protected override bool FuzeActive()
+    {
+        return _phase == LoiteringFlightPhase.Attacking && base.FuzeActive();
+    }
+
+    protected override void OnUnpooled()
+    {
+        base.OnUnpooled();
+        ResetLoiteringState();
+    }
+
+    protected override void OnRepooled()
+    {
+        ResetLoiteringState();
+        base.OnRepooled();
+    }
+
+    private void EnterPhase(LoiteringFlightPhase phase)
+    {
+        _phase = phase;
+        _phaseTime = 0f;
+
+        if (phase is LoiteringFlightPhase.Arming or LoiteringFlightPhase.Loitering)
+        {
+            EngineOff();
+            _body.drag = _loiterDrag;
+        }
+        else if (phase == LoiteringFlightPhase.Attacking)
+        {
+            _body.drag = _defaultDrag;
+            EngineOn();
+            ResetFlightTime();
+        }
+    }
+
+    private bool TryTriggerMagneticFuse()
+    {
+        if (TryTriggerSelectedSeekerFuse())
+        {
+            return true;
+        }
+
+        if (!ProximityShips && !ProximityFighters && !ProximityMissiles)
+        {
+            return false;
+        }
+
+        int count = Physics.OverlapSphereNonAlloc(
+            transform.position,
+            _activationRange,
+            _activationResults,
+            _activationLayers,
+            QueryTriggerInteraction.Collide);
+
+        for (int i = 0; i < count; i++)
+        {
+            Transform targetRoot = _activationResults[i]?.transform.root;
+            if (targetRoot == null)
+            {
+                continue;
+            }
+
+            if (ProximityShips)
+            {
+                ShipController ship = targetRoot.GetComponent<ShipController>();
+                if (ship != null && !ship.IsEliminated && !ship.GetIFF(OwnedBy).IsFriendly() && HasActivationLineOfSight(ship.Position))
+                {
+                    return true;
+                }
+            }
+
+            if (ProximityFighters)
+            {
+                Spacecraft fighter = targetRoot.GetComponent<Spacecraft>();
+                if (fighter != null && !fighter.GetIFF(OwnedBy).IsFriendly() && HasActivationLineOfSight(fighter.Position))
+                {
+                    return true;
+                }
+            }
+
+            if (ProximityMissiles)
+            {
+                Missile missile = targetRoot.GetComponent<Missile>();
+                if (missile != null && missile != this && !IFFExtensions.GetIFF(missile.OwnedBy, OwnedBy).IsFriendly() && HasActivationLineOfSight(missile.transform.position))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private bool TryTriggerSelectedSeekerFuse()
+    {
+        RuntimeMissileSeeker[] seekers = GetComponentsInChildren<RuntimeMissileSeeker>();
+        IReadOnlyList<RuntimeMissileSeeker> noValidators = Array.Empty<RuntimeMissileSeeker>();
+
+        foreach (RuntimeMissileSeeker seeker in seekers)
+        {
+            int socketIndex = seeker.Descriptor.Socket.Index;
+            if (socketIndex < 0 || socketIndex >= SeekerFuses.Count || !SeekerFuses[socketIndex])
+            {
+                continue;
+            }
+
+            RuntimeMissileSeeker.SeekerSearchResult result = seeker.SearchForTarget(
+                noValidators,
+                out _,
+                out _,
+                out _);
+            if (result == RuntimeMissileSeeker.SeekerSearchResult.Found)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void ExtendSeekerFuseList()
+    {
+        while (SeekerFuses.Count < Sockets.Count)
+        {
+            SeekerFuses.Add(false);
+        }
+    }
+
+    private bool HasActivationLineOfSight(Vector3 targetPosition)
+    {
+        return !_requireLineOfSight || !Physics.Linecast(transform.position, targetPosition, _obstructionLayers);
+    }
+
+    private void ResetLoiteringState()
+    {
+        _phase = LoiteringFlightPhase.Inactive;
+        _phaseTime = 0f;
+        if (_body != null)
+        {
+            _body.drag = _defaultDrag;
+        }
+    }
+}
